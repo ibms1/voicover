85c30a6ce1258f599d32dc2e127d4e7f04500eab
ibms1 <ibms2000@gmail.com>
2024-12-04 09:03:59 +0300

    F  commit

--- voiceover.py
+++ voiceover.py
@@ -1,15 +1,6 @@
 import streamlit as st
 from gtts import gTTS
 import os
-from pydub import AudioSegment
-from pydub.utils import which
-
-# Ensure ffmpeg is available
-ffmpeg_path = which("ffmpeg")
-if ffmpeg_path is None:
-    st.error("Please Try Again Later")
-else:
-    AudioSegment.converter = ffmpeg_path
 
 # Function to convert text to speech using gTTS
 def text_to_speech(text, lang):
@@ -29,28 +20,8 @@ if st.button("Convert Text to Speech"):
         st.error("Please enter some text.")
     else:
         output_file = text_to_speech(text, lang)
-        try:
-            audio = AudioSegment.from_mp3(output_file)
-            st.audio(output_file, format="audio/mp3")
-        except Exception as e:
-            st.error(f"Error loading audio file: {e}")
+        st.audio(output_file, format="audio/mp3")
     
     # Optional: Delete the audio file after use
-    if os.path.exists("output.mp3"):
-        os.remove("output.mp3")
-# CSS مخصص لإخفاء الروابط عند تمرير الفأرة
-hide_links_style = """
-    <style>
-    a {
-        text-decoration: none;
-        color: inherit;
-        pointer-events: none;
-    }
-    a:hover {
-        text-decoration: none;
-        color: inherit;
-        cursor: default;
-    }
-    </style>
-    """
-st.markdown(hide_links_style, unsafe_allow_html=True)
+    if os.path.exists(output_file):
+        os.remove(output_file)
